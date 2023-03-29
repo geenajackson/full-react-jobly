bf9a9e1e5ebbd10c74a33f6c530847504237ebae
Geena Jackson <geena.jackson96@gmail.com>
2023-03-29 11:38:48 -0500

    create basic component structure

---
+++ frontend/src/Company.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Company() {
+    return (
+        <div></div>
+    )
+}
+
+export default Company;
---
+++ frontend/src/Job.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Job() {
+    return (
+        <div></div>
+    )
+}
+
+export default Job;
