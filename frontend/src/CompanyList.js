325e3d673137647ed99c69b5f555956413f57860
Geena Jackson <geena.jackson96@gmail.com>
2023-03-29 12:01:36 -0500

    add state and fetchData func for retrieving companies

--- frontend/src/CompanyList.js
+++ frontend/src/CompanyList.js
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import JoblyApi from "./api"
+import Company from "./Company"
 
 function CompanyList() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [companies, setCompanies] = useState([]);
+
+    async function fetchData() {
+        setIsLoading(true);
+        async function getCompanies() {
+            let companies = await JoblyApi.getCompanies();
+            setCompanies(companies);
+            setIsLoading(false);
+        };
+        getCompanies();
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    if (isLoading) {
+        return <p>Loading ...</p>;
+    }
+
     return (
-        <div></div>
+        <div>
+            {companies.map(company => (
+                <Company>{company}</Company>
+            ))}
+        </div>
     )
 }
 
