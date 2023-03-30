c07080d12690128773f560ff8b8d3a695503bad9
Geena Jackson <geena.jackson96@gmail.com>
2023-03-30 14:33:23 -0500

    add helper funcs for calling JoblyApi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,40 @@
+import React, { useState } from "react";
 import Routes from "./Routes"
 import './App.css';
 
+import UserContext from "./userContext";
+import NavBar from "./NavBar";
+import JoblyApi from "./api";
+
+
 function App() {
+  const [user, setUser] = useState("");
+
+  async function loginUser(login) {
+    let token = await JoblyApi.getToken(login);
+    console.log(token)
+    setUser(token);
+  }
+
+  async function registerUser(login) {
+    let token = await JoblyApi.register(login);
+    setUser(token);
+  }
+
+  function logoutUser() {
+    JoblyApi.logout();
+    setUser(null);
+  }
+
+  let userToken = JoblyApi.token;
+
   return (
     <div className="App">
-      <Routes />
+      <p>hi! {userToken}</p>
+      <UserContext.Provider value={user}>
+        <NavBar />
+        <Routes loginUser={loginUser} logoutUser={logoutUser} registerUser={registerUser} />
+      </UserContext.Provider>
     </div>
   );
 }
