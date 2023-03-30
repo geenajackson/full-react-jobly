1ee62779a160ce937edf16162fc3b5fd5fa2b517
Geena Jackson <geena.jackson96@gmail.com>
2023-03-30 14:31:48 -0500

    display list of jobs and implement search

--- frontend/src/JobList.js
+++ frontend/src/JobList.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import JoblyApi from "./api"
+import SearchBar from "./SearchBar";
+import Job from "./Job"
 
 function JobList() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [jobs, setJobs] = useState([]);
+
+    async function fetchData(query = undefined) {
+        setIsLoading(true);
+        async function getJobs() {
+            let jobs = await JoblyApi.getJobs(query);
+            setJobs(jobs);
+            setIsLoading(false);
+        };
+        getJobs();
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    if (isLoading) {
+        return <p>Loading ...</p>;
+    }
+
     return (
-        <div></div>
+        <div>
+            <SearchBar fetchData={fetchData} />
+            {jobs.map(job => (
+                <Job
+                    key={job.id}
+                    title={job.title}
+                    salary={job.salary}
+                    equity={job.equity}
+                    companyName={job.companyName}
+                ></Job>
+            ))}
+        </div>
     )
 }
 
