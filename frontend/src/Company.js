21cc2466dea05fe1518fd90b14d1e616d2de770e
Geena Jackson <geena.jackson96@gmail.com>
2023-03-30 14:32:52 -0500

    display basic company information

--- frontend/src/Company.js
+++ frontend/src/Company.js
@@ -1,8 +1,13 @@
 import React from "react";
 
-function Company() {
+function Company({ name, description, numEmployees, logoUrl }) {
     return (
-        <div></div>
+        <div>
+            <h2>{name}</h2>
+            {logoUrl ? <img src={logoUrl} alt={name}></img> : ""}
+            <p>{description}</p>
+            <p>Number of Employees: {numEmployees}</p>
+        </div>
     )
 }
 
