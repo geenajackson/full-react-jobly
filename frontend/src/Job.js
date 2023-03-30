03550a0fa1858d2f036926da8d7aa2941c46ece7
Geena Jackson <geena.jackson96@gmail.com>
2023-03-30 14:32:04 -0500

    display basic job information

--- frontend/src/Job.js
+++ frontend/src/Job.js
@@ -1,8 +1,15 @@
 import React from "react";
 
-function Job() {
+function Job({ title, salary, equity, companyName }) {
     return (
-        <div></div>
+        <div>
+            <h2>{title}</h2>
+            <ul>
+                <li>Company: {companyName}</li>
+                <li>Salary: {salary}</li>
+                {equity ? <li>Equity: {equity}</li> : ""}
+            </ul>
+        </div>
     )
 }
 
