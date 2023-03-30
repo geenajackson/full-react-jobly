9392855236c3acb83bd771b14136a5e0b1e56581
Geena Jackson <geena.jackson96@gmail.com>
2023-03-30 14:31:23 -0500

    create links to login/signup forms

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 function Login() {
     return (
-        <div></div>
+        <div>
+            <Link to="/login/form">Login!</Link>
+            <Link to="/signup">Sign Up!</Link>
+        </div>
     )
 }
 
