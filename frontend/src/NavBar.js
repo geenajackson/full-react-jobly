7b658677b84b04b7db4d41f97ee219760d0df0dd
Geena Jackson <geena.jackson96@gmail.com>
2023-04-03 11:32:49 -0500

    add conditional for logged in users

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 
+import UserContext from "./userContext";
+
+//put multiple NavLinks in an array inside of the conditional
+
 function NavBar() {
+    const user = useContext(UserContext);
     return (
         <div>
             <NavLink exact to="/">
@@ -16,12 +21,16 @@ function NavBar() {
             <NavLink exact to="/profile">
                 Profile
             </NavLink>
-            <NavLink exact to="/login">
-                Login
-            </NavLink>
-            <NavLink exact to="/signup">
-                Sign Up
-            </NavLink>
+            {user ?
+                <NavLink exact to="/logout">Logout</NavLink> :
+                [<NavLink key="login" exact to="/login">
+                    Login
+                </NavLink>,
+                <NavLink key="signup" exact to="/signup">
+                    Sign Up
+                </NavLink>]
+            }
+
         </div>
     )
 }
