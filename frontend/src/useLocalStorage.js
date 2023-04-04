ad467efec8a9e90cc74401ab714b05b8757e8123
Geena Jackson <geena.jackson96@gmail.com>
2023-04-04 17:08:26 -0500

    create localStorage hook

---
+++ frontend/src/useLocalStorage.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+function useLocalStorage(key, firstValue = null) {
+    const initialValue = localStorage.getItem(key) || firstValue;
+    const [value, setValue] = useState(initialValue);
+
+    useEffect(() => {
+        if (value === null) {
+            localStorage.removeItem(key);
+        } else {
+            localStorage.setItem(key, value);
+        }
+    }, [key, value]);
+    console.log(value)
+    return [value, setValue]
+
+}
+
+export default useLocalStorage;
