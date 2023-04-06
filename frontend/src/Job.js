ddade9c6ba9aa458f8414ed28eaaf4965f4266ba
Geena Jackson <geena.jackson96@gmail.com>
2023-04-06 13:59:41 -0500

    add handleSubmit func for clicking on apply button

--- frontend/src/Job.js
+++ frontend/src/Job.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-function Job({ title, salary, equity, companyName }) {
+function Job({ jobId, title, salary, equity, companyName, applyUser, applications }) {
+    const history = useHistory();
+    async function handleSubmit(evt) {
+        evt.preventDefault();
+        let res = await applyUser(jobId);
+        console.log(res);
+        history.push("/jobs")
+    }
+
     return (
         <div>
             <h2>{title}</h2>
@@ -9,6 +18,8 @@ function Job({ title, salary, equity, companyName }) {
                 <li>Salary: {salary}</li>
                 {equity ? <li>Equity: {equity}</li> : ""}
             </ul>
+            {applications.includes(jobId) ? <button>Applied!</button> :
+                <button onClick={handleSubmit}>Apply!</button>}
         </div>
     )
 }
