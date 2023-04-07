14172abb2ddfe35cfd674e27ba1b395a65fbc205
Geena Jackson <geena.jackson96@gmail.com>
2023-04-07 13:38:00 -0500

    update styles of navbar

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -1,5 +1,19 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import {
+    //     Collapse,
+    Navbar,
+    //     // NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink as Navlink
+    //     // UncontrolledDropdown,
+    //     // DropdownToggle,
+    //     // DropdownMenu,
+    //     // DropdownItem,
+    //     // NavbarText,
+} from 'reactstrap';
 
 import UserContext from "./userContext";
 
@@ -9,29 +23,59 @@ function NavBar() {
     const user = useContext(UserContext);
     return (
         <div>
-            <NavLink exact to="/">
-                Jobly
-            </NavLink>
-            <NavLink exact to="/companies">
-                Companies
-            </NavLink>
-            <NavLink exact to="/jobs">
-                Jobs
-            </NavLink>
+            <Navbar color="dark">
+                <NavbarBrand>
+                    <NavLink exact to="/">
+                        Jobly
+                    </NavLink>
+                </NavbarBrand>
+                <Nav>
+                    <NavItem>
+                        <Navlink>
+                            <NavLink exact to="/companies">
+                                Companies
+                            </NavLink>
+                        </Navlink>
+                    </NavItem>
+                    <NavItem>
+                        <Navlink>
+                            <NavLink exact to="/jobs">
+                                Jobs
+                            </NavLink>
+                        </Navlink>
+                    </NavItem>
 
-            {user ?
-                [<NavLink key="profile" exact to="/profile">
-                    Profile
-                </NavLink>,
-                <NavLink key="logout" exact to="/logout">Logout</NavLink>] :
-                [<NavLink key="login" exact to="/login">
-                    Login
-                </NavLink>,
-                <NavLink key="signup" exact to="/signup">
-                    Sign Up
-                </NavLink>]
-            }
 
+                    {user ?
+                        [<NavItem>
+                            <Navlink>
+                                <NavLink key="profile" exact to="/profile">
+                                    Profile
+                                </NavLink>
+                            </Navlink>
+                        </NavItem>,
+                        <NavItem>
+                            <Navlink>
+                                <NavLink key="logout" exact to="/logout">Logout</NavLink>
+                            </Navlink>
+                        </NavItem>] :
+                        [<NavItem>
+                            <Navlink>
+                                <NavLink key="login" exact to="/login">
+                                    Login
+                                </NavLink>
+                            </Navlink>
+                        </NavItem>,
+                        <NavItem>
+                            <Navlink>
+                                <NavLink key="signup" exact to="/signup">
+                                    Sign Up
+                                </NavLink>
+                            </Navlink>
+                        </NavItem>]
+                    }
+                </Nav>
+            </Navbar>
         </div>
     )
 }
